77ef78b06932ddd8275ce9d3674afbc46c161e25
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-06-22 16:10:33 -0500

    dependecy nodemon was added, logic express was implemented.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 import express from "express";
+
+const app = express();
+
+app.get('/', (req, res) => res.send("Bienvenido a mi API"));
+
+app.get("/employees", (req, res) => res.send("Obteniendo los empleados"));
+app.post("/employees", (req, res) => res.send("Creando los empleados"));
+app.put("/employees", (req, res) => res.send("Editando los empleados"));
+app.delete("/employees", (req, res) => res.send("Eliminando los empleados"));
+
+app.listen(3000);
+
+console.log("esta ejecutando en el puerto 3000")
